f978f6e377f4564022d8ec9fe58d6895b3489ff3
jhanisusM <jhanisus@gmail.com>
2018-05-11 20:53:22 -0700

    update #2

--- greatBay.js
+++ greatBay.js
@@ -1,13 +1,32 @@
 var mysql = require("mysql");
+var inquirer = require('inquirer');
+var password;
+
+// inquirer.prompt([
+//     {
+//         type: "password",
+//         message: "Please enter your root password",
+//         name: "password"
+//     }
+// ]).then(function (answers) {
+//     password = answers.password;
+//     console.log(typeof password);
+// }).then(function () {});
 
 var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-
-  // Your username
-  user: "root",
-
-  // Your password
-  password: "cali",
-  database: "greatBay"
+    host: "localhost",
+    port: 3306,
+    // Your username
+    user: "root",
+    // Your password
+    password: "cali",
+    database: "greatBay"
 });
+
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected as id " + connection.threadId);
+    connection.end();
+});
+
